f38b97bd61756b64f932e8089fdcdb5f64026cf5
candicenoel <candice29@gmail.com>
2019-11-12 21:43:26 +0000

    Video uploading

--- Gemfile
+++ Gemfile
@@ -67,8 +67,9 @@ gem 'bootstrap', '4.0.0.alpha6'
 
 gem 'simple_form'
 gem 'devise'
-gem 'carrierwave', '~> 2.0'
+gem 'carrierwave', '~> 2.0.2'
 gem 'mini_magick'
+gem 'carrierwave-aws'
 
 source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_10_021517) do
+ActiveRecord::Schema.define(version: 2019_11_12_212818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2019_11_10_021517) do
     t.integer "section_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "video"
     t.index ["section_id"], name: "index_lessons_on_section_id"
   end
 
