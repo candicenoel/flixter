01620d0395e929f6dbe66ad7120ee6682f90747a
candicenoel <candice29@gmail.com>
2019-11-14 01:55:26 +0000

    New section form modal

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -16,11 +16,10 @@ class Instructor::CoursesController < ApplicationController
   end
 
   def show
-    @photo = Photo.new
+    @section = Section.new
   end
 
   private
-
   def require_authorized_for_current_course
     if current_course.user != current_user
       render plain: "Unauthorized", status: :unauthorized
